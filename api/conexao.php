a4a9080353daf1bc96155c75a3127ebd0b473fc3
lucas-geremias <geremiaslucas2012@gmail.com>
2025-04-19 23:03:22 -0300

    banco de dados

--- api/conexao.php
+++ api/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";  // Servidor do MySQL (padrão: localhost)
-$usuario = "root";    // Usuário padrão do MySQL no XAMPP
-$senha = "";          // Senha padrão (vazia no XAMPP)
-$banco = "ocorrencias"; // Nome do banco de dados
+$host = "seu-banco-de-dados.render.com";  // Substitua pelo host do banco de dados fornecido pelo Render
+$usuario = "seu-usuario";                 // Usuário do banco de dados fornecido pelo Render
+$senha = "sua-senha";                     // Senha do banco de dados fornecida pelo Render
+$banco = "ocorrencias";                   // Nome do banco de dados
 
 // Criar conexão
 $conn = new mysqli($host, $usuario, $senha, $banco);
